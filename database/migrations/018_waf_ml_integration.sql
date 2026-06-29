1583a7043a6e4086b59aacbf509f35ef080af947
Coder-MoeTain <emptysky00@gmail.com>
2026-06-29 08:47:01 +0630

    update

--- database/migrations/018_waf_ml_integration.sql
+++ database/migrations/018_waf_ml_integration.sql
@@ -1,9 +1,9 @@
 -- WebGuard ML integration settings for WAF
 
-INSERT INTO waf_settings (setting_key, setting_value, setting_type) VALUES
-('ml_waf_enabled', 'false', 'boolean'),
-('ml_waf_confidence_threshold', '0.7', 'number'),
-('ml_waf_model_id', '', 'string'),
-('ml_waf_block_standalone', 'true', 'boolean'),
-('ml_waf_reject_uncertain', 'true', 'boolean')
+INSERT INTO waf_settings (setting_key, setting_value, setting_type, created_at, updated_at) VALUES
+('ml_waf_enabled', 'false', 'boolean', NOW(), NOW()),
+('ml_waf_confidence_threshold', '0.7', 'number', NOW(), NOW()),
+('ml_waf_model_id', '', 'string', NOW(), NOW()),
+('ml_waf_block_standalone', 'true', 'boolean', NOW(), NOW()),
+('ml_waf_reject_uncertain', 'true', 'boolean', NOW(), NOW())
 ON DUPLICATE KEY UPDATE setting_key = VALUES(setting_key);
